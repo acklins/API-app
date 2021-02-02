02b8c75fd715e3a2768c12c2cd1992673e44dd34
Della <dmcmillan100@gmnail.com>
2021-02-02 14:39:17 -0800

    JS code working

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
-# API-app
-testing to see if text appears*
+# API-
+This README.md file includes the following sections:
+
+An embedded screenshot of the app
+ Reddit Search API
+ Requests data from the Reddit API and displays that data on the main Homepage.
+ Creating a single page site that retrieves data from an API and displays it to a user.
+
+ List of the Technologies used:
+HTML, CSS, JS, git, vscode, useage of the DOM, HTML5 Validator and CSS Validator
+
+ Installation instructions
+
+
+ Your User stories – who are your users, what do they want and why?
+
+ Your Wireframes – sketches of major views / interfaces in your application
+
+ Descriptions of any Unsolved problems or major hurdles I had to overcome
+
+
--- js/main.js
+++ js/main.js
@@ -1,1 +1,44 @@
-console.log('Sanity Check')
+const itemList = document.querySelector("#itemList");
+
+const form = document.querySelector("form");
+
+form.addEventListener("submit", (evt) => {
+  evt.preventDefault();
+  let input = document.querySelector("#input");
+});
+const requestUrl = "https://www.reddit.com/search.json?q=";
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  fetch(requestUrl + input.value)
+    .then((responseData) => {
+      return responseData.json();
+    })
+    .then((jsonData) => {
+      let items = jsonData.data.children;
+      console.log(jsonData);
+      addItem(items[0]);
+    })
+    .catch((error) => {
+      console.log("Oops, think we have an error.", error);
+    });
+});
+let li = document.createElement("li");
+
+const addItem = (item) => {
+  const html = ` <ul>
+            <li>title: ${item.data.title}</li>
+            <li>fallback_url: ${item.data.secure_media.reddit_video.fallback_url}</li>
+            <li>thumbnail: <img src=" ${item.data.thumbnail}"></li>
+                </ul>
+                `;
+  li.innerHTML = html;
+  itemList.appendChild(li);
+};
+const clearAddItem = (event) => {
+    console.log(event)
+             event.target.innerHTML =  "";
+  itemList.appendChild(li);
+};
+document.querySelector("ul").addEventListener("click", clearAddItem);
+clearAddItem(itemList);
