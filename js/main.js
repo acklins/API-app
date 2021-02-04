270216772e8220d9c6ab6389a1a1c68054283c68
Della <dmcmillan100@gmnail.com>
2021-02-04 14:16:38 -0800

    last commit

--- js/main.js
+++ js/main.js
@@ -1,15 +1,16 @@
 const itemList = document.querySelector("#itemList");
 
 const form = document.querySelector("form");
-//fetching data from an api
+
+//fetching data from an api, this function executes when submit is pressed
 form.addEventListener("submit", (evt) => {
   evt.preventDefault();
   let input = document.querySelector("#input");
 });
 const requestUrl = "https://www.reddit.com/search.json?q=";
 
-//the form is listening for the click and returns the json response from the api
-//the json is parsed only returning 3 needed portions of the entire response
+//the form is listening for the click and returns the fallback, thumbnail & title from
+//the reddit api
 form.addEventListener("submit", (e) => {
   e.preventDefault();
   fetch(requestUrl + input.value)
@@ -21,11 +22,12 @@ form.addEventListener("submit", (e) => {
       addItem(items[0]);
     })
     .catch((error) => {
-      //console.log("Oops, think we have an error.", error);
+      //console.error("Oops, think we have an error.", error);
     });
 });
 let li = document.createElement("li");
-//adds the items to the page
+
+//adding the title, fallback & thumbnail to the page
 const addItem = (item) => {
   const html = ` <ul>
             <li>TITLE: ${item.data.title}</li>
@@ -36,13 +38,13 @@ const addItem = (item) => {
   li.innerHTML = html;
   itemList.appendChild(li);
 };
-//this function will remove the json items sequentially from the page
+//this function will remove the elements displayed sequentially from the page
 const clearAddItem = (event) => {
   while (event.target.firstChild) {
     event.target.removeChild(event.target.lastChild);
   }
 };
-//removes items when click is enabled
+//any item from the search when clicked can be removed
 document.querySelector("ul").addEventListener("click", (e) => {
   clearAddItem(e);
   if(e.target.children.length === 0){
